4f4db44ac5d45bc2b66cba559e6de868c7849388
azirulislam <azirulislam27@gmail.com>
2022-04-17 10:41:51 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/About/About';
+import Services from './components/Services/Services';
+import About from './components/About/About';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register'
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hellow world</h2>
-      
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/Home' element={<Home></Home>}></Route>
+        <Route path='/Services' element={<Services></Services>}></Route>
+        <Route path='/About' element={<About></About>}></Route>
+        <Route path='/Login' element={<Login></Login>}></Route>
+        <Route path='/Register' element={<Register></Register>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <nav className='header'>
+        <div>
+            <Link to="/Home">Home</Link>
+            <Link to="/Services">Services</Link>
+            <Link to="/About">About</Link>
+            <Link to="/Login">Login</Link>
+            <Link to="/Register">Register Now</Link>
+        </div>
+    </nav>
+    );
+};
+
+export default Header;
